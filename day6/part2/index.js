50fcf7f2bc281e1c9645864da308370503ec2cd8
Bryan Veneruso <bryanv@qualtrics.com>
2020-12-05 21:26:09 -0800

    Refactoring for simplicity

--- day6/part2/index.js
+++ day6/part2/index.js
@@ -1,30 +1,20 @@
 const lineReader = require('line-reader');
 const Promise = require('bluebird');
 const eachLine = Promise.promisify(lineReader.eachLine);
-let saidYesTo = {};
+let saidYesTo = [];
 
 let parseLine = function(line, isFirstLine) {
 
-    if(isFirstLine) {
+    let set = [];
+    for (let i = 0; i < line.length; i++) {
+        set.push(line.charAt(i));
+    }
 
-        // If first element in group, initialize said yes to char array
-        for (let i = 0; i < line.length; i++) {
-            let char = line.charAt(i);
-            saidYesTo[char] = 1;
-        }
+    if(isFirstLine) {
+        saidYesTo = set;
         isFirst = false;
     } else {
-
-        // If this isn't the first element, we should invalidate any question that this entry didn't say yes to
-        let keysToDelete = [];
-        for (let key of Object.keys(saidYesTo)) {
-            if(line.indexOf(key) == -1) {
-                keysToDelete.push(key);
-            }
-        }
-        for(let key of keysToDelete) {
-            delete saidYesTo[key];
-        }
+        saidYesTo = saidYesTo.filter(val => set.includes(val));
     }
 };
 
@@ -32,14 +22,14 @@ let sum = 0;
 let isFirst = true;
 eachLine('./input.txt', function(line) {
     if(line.trim().length == 0) {
-        sum += Object.keys(saidYesTo).length;
+        sum += saidYesTo.length;
         saidYesTo = {};
         isFirst = true;
     } else {
         parseLine(line.trim(), isFirst);
     }
 }).then(function(err) {
-    sum += Object.keys(saidYesTo).length;
+    sum += saidYesTo.length;
     console.log(sum);
 });
 
