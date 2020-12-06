0a63cd5521d396b0e6dd6eb10ed7d1cc36c2f67d
Bryan Veneruso <bryanv@qualtrics.com>
2020-12-05 21:34:42 -0800

    More refactoring

--- day6/part2/index.js
+++ day6/part2/index.js
@@ -2,31 +2,27 @@ const lineReader = require('line-reader');
 const Promise = require('bluebird');
 const eachLine = Promise.promisify(lineReader.eachLine);
 let saidYesTo = [];
+let sum = 0;
+let isFirst = true;
 
-let parseLine = function(line, isFirstLine) {
-
+let parseLine = function(line) {
     let set = [];
     for (let i = 0; i < line.length; i++) {
         set.push(line.charAt(i));
     }
 
-    if(isFirstLine) {
-        saidYesTo = set;
-        isFirst = false;
-    } else {
-        saidYesTo = saidYesTo.filter(val => set.includes(val));
-    }
+    saidYesTo = isFirst ? set : saidYesTo.filter(val => set.includes(val));
+    isFirst = false;
 };
 
-let sum = 0;
-let isFirst = true;
+
 eachLine('./input.txt', function(line) {
     if(line.trim().length == 0) {
         sum += saidYesTo.length;
         saidYesTo = {};
         isFirst = true;
     } else {
-        parseLine(line.trim(), isFirst);
+        parseLine(line.trim());
     }
 }).then(function(err) {
     sum += saidYesTo.length;
