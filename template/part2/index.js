e496f681c46a25c0b2454171d68cebcc7b278100
Bryan Veneruso <bryanv@qualtrics.com>
2020-12-06 18:25:02 -0800

    Add template and empty day7

---
+++ template/part2/index.js
@@ -0,0 +1,10 @@
+const utils = require('../../utils/utils');
+
+let handleEachLine = function(line, lineCount, lineInGroup) {
+
+};
+
+utils.parseFile('./input.txt', handleEachLine, null, function() {
+
+});
+
